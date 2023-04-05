d93d06d0ba9f3d16aed16186b41e00ca09188a1b
gonza <cardoso63263@estudiantes.edu.ar>
2023-04-05 09:27:12 -0300

    ejercicios hechos

--- ejercicios/letras.go
+++ ejercicios/letras.go
@@ -2,8 +2,16 @@ package ejercicios
 
 import (
 	"guia4/set"
+
+	"github.com/agrison/go-commons-lang/stringUtils"
 )
 
 func Letras(s string) *set.Set[string] {
-	panic("Not implemented")
+	conjunto := set.NewSet[string]()
+	for _, v := range s {
+		if !stringUtils.IsBlank(string(v)) {
+			conjunto.Add(string(v))
+		}
+	}
+	return conjunto
 }
